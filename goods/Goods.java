a337913a2bcc195d9c6a885db81cc85eaa4ce068
mirub <miru.b99@gmail.com>
2020-03-10 15:35:59 +0200

    added goods folder

---
+++ goods/Goods.java
@@ -0,0 +1,39 @@
+package com.tema1.goods;
+
+import java.util.Map;
+
+public abstract class Goods {
+    private final int id;
+    private final GoodsType type;
+    private final int profit;
+    private final int penalty;
+
+    public Goods(final int id, final GoodsType type, final int profit, final int penalty) {
+        this.id = id;
+        this.type = type;
+        this.profit = profit;
+        this.penalty = penalty;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final GoodsType getType() {
+        return type;
+    }
+
+    public final int getProfit() {
+        return profit;
+    }
+
+    public final int getPenalty() {
+        return penalty;
+    }
+
+    public abstract Map<Goods, Integer> getIllegalBonus();
+
+    public abstract int getKingBonus();
+
+    public abstract int getQueenBonus();
+}
---
+++ goods/IllegalGoods.java
@@ -0,0 +1,25 @@
+package com.tema1.goods;
+
+import java.util.Map;
+
+public final class IllegalGoods extends Goods {
+    private final Map<Goods, Integer> illegalBonus;
+
+    public IllegalGoods(final int id, final int profit,
+                        final int penalty, final Map<Goods, Integer> illegalBonus) {
+        super(id, GoodsType.Illegal, profit, penalty);
+        this.illegalBonus = illegalBonus;
+    }
+
+    public Map<Goods, Integer> getIllegalBonus() {
+        return illegalBonus;
+    }
+
+    public int getKingBonus() {
+        return 0;
+    }
+
+    public int getQueenBonus() {
+        return 0;
+    }
+}
